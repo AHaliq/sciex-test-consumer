fce390b7b0e6731d649003b770782e9fc2d3716f
AHaliq <liqo12@hotmail.com>
2020-04-17 18:44:36 +0800

    disable error

--- stc/writers/excel.py
+++ stc/writers/excel.py
@@ -9,6 +9,6 @@ import pandas as pd
 
 
 def write_frame_to_new_excel(path, file_name, frame):
-    writer = pd.ExcelWriter(f'{path}/{file_name}.xlsx', engine='xlsxwriter')
-    frame.to_excel(writer, sheet_name='Sheet1')
-    writer.save()
+    with pd.ExcelWriter(f'{path}/{file_name}.xlsx', engine='xlsxwriter') as xl_writer:  # pylint: disable=abstract-class-instantiated
+        frame.to_excel(xl_writer, sheet_name='Sheet1')
+        xl_writer.save()

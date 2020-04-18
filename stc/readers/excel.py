31b3a3b4012e3349bb1cd5850de3eaefff9cc9e0
AHaliq <liqo12@hotmail.com>
2020-04-18 18:26:10 +0800

    change to red color

--- stc/readers/excel.py
+++ stc/readers/excel.py
@@ -6,8 +6,9 @@ Read excel files util
 """
 
 import pandas as pd
+import functools
 
 
-def excel_sheet_to_frame(excel_path, sheet):
+def excel_sheet_to_frame(excel_path, sheet, columns):
     full_frame = pd.read_excel(excel_path, sheet_name=sheet)
-    return (full_frame.iloc[:, 0].astype(str) + full_frame.iloc[:, 1].astype(str)).str.cat(sep='\n')
+    return functools.reduce(lambda s: s + s, [full_frame.iloc[:, x].astype(str) for x in columns]).str.cat(sep='\n')
